ab58ef3295cb25ac2859134155aa23d843064722
Ameer <ameerulislam@gmail.com>
2022-05-28 11:55:28 +0600

    Initial commit

---
+++ test/cdk-ts-pluralsight.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTsPluralsight from '../lib/cdk-ts-pluralsight-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ts-pluralsight-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTsPluralsight.CdkTsPluralsightStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
